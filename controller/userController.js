a159f29ad2c02c2a9b51d473c735967961894aa7
ridowayne <hassanrilwan12@gmail.com>
2022-10-27 14:22:15 +0100

    first commit

--- controller/userController.js
+++ controller/userController.js
@@ -1,13 +1,12 @@
 exports.myInfo = async (req, res) => {
   try {
-    const userInfo = [
-      {
-        slasckUserName: "@Rilwan",
-        backend: true,
-        age: 25,
-        bio: "Well I cook anything i am told to cook, agba beans cooker",
-      },
-    ];
+    const userInfo = {
+      slasckUserName: "@Rilwan",
+      backend: true,
+      age: 25,
+      bio: "Well I cook anything i am told to cook, agba beans cooker",
+    };
+
     res.status(200).json(userInfo);
   } catch (error) {
     console.log(error);
