0d59b499d7c366512433d218431c161f04a70790
ridowayne <hassanrilwan12@gmail.com>
2022-10-27 11:52:40 +0100

    first commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("welcome to bfree feedback app, BEA");
+  res.send("welcome HOME page of my first hng task");
 });
 app.use("/api/v1/test", user);
 const port = process.env.PORT || 3000;
